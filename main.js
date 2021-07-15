a649a62cbf36fc1540c7e342c89358d969e0aea9
Luciano <lucco6823@gmail.com>
2021-07-15 17:37:05 +0200

    Aggiunta prezzo da pagare

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-var km = prompt("Inserisci il numero di chilometri che vuoi percorrere: ");
-var eta = prompt("Inserisci la tua età: ");
+var km = parseInt(prompt("Inserisci il numero di chilometri che vuoi percorrere: "));
+var eta = parseInt(prompt("Inserisci la tua età: "));
 
 console.log(km);
 console.log(eta);
+
+var prezzo = km * 0.21;
+console.log(prezzo);
